98a495d2cad1cbafa1f37ff4d5e2105897211d9f
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-06-04 12:57:13 +0200

    add styling to cards

--- routes/index.js
+++ routes/index.js
@@ -19,12 +19,15 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
-router.get('/new', function (req, res, next){
-  res.render('form')
-})
+router.get('/new', function (req, res, next) {
+  res.render('form');
+});
 
 router.post('/new', function (req, res, next) {
-  
-})
+  const name = req.body.name;
+  const message = req.body.message;
+  messages.push({ text: message, user: name, added: new Date() });
+  res.redirect('/')
+});
 
 module.exports = router;
